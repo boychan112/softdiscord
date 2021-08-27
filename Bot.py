566f1cbcd361abc218e4ec9f246d7930492fd239
won_chan <83020544+boychan112@users.noreply.github.com>
2021-08-27 11:17:03 +0900

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -44,7 +44,7 @@ async def on_message(message):
         await message.channel.send(embed=embed)
 
     if content.startswith("!월시간표"):
-        embed=discord.Embed(description="<1교시>\n프로\n\n<2교시>\n프로\n\n<3교시>\n프로\n\n<4교시>\n프로\n\n<5교시>\n수학\n\n<6교시>\n사회\n이용각선생님", color=0x00ff56)
+        embed=discord.Embed(description="<1교시>\n프로\n임미경선생님\n\n<2교시>\n프로\n임미경선생님\n\n<3교시>\n프로\n임미경선생님\n\n<4교시>\n프로\n임미경선생님\n\n<5교시>\n수학\n\n<6교시>\n사회\n이용각선생님", color=0x00ff56)
         embed.set_author(name="1-9 월요일 시간표", url="https://cdn.discordapp.com/attachments/873384182997479457/879342240839905360/unknown.png")
         embed.set_image(url="https://cdn.discordapp.com/attachments/873384182997479457/879342240839905360/unknown.png")
         await message.channel.send(embed=embed)
@@ -62,7 +62,7 @@ async def on_message(message):
         await message.channel.send(embed=embed)
 
     if content.startswith("!목시간표"):
-        embed=discord.Embed(description="<1교시>\n과학\n\n<2교시>\n프로\n\n<3교시>\n프로\n\n<4교시>\n체육\n\n<5교시>\n국어\n\n<6교시>\n영어\n\n<7교시>\n사회\n이진경선생님", color=0x00ff56)
+        embed=discord.Embed(description="<1교시>\n과학\n\n<2교시>\n프로\n박진영선생님\n\n<3교시>\n프로\n박진영선생님\n\n<4교시>\n체육\n\n<5교시>\n국어\n\n<6교시>\n영어\n\n<7교시>\n사회\n이진경선생님", color=0x00ff56)
         embed.set_author(name="1-9 월요일 시간표", url="https://cdn.discordapp.com/attachments/873384182997479457/879342240839905360/unknown.png")
         embed.set_image(url="https://cdn.discordapp.com/attachments/873384182997479457/879342240839905360/unknown.png")
         await message.channel.send(embed=embed)
