f860b71a9d37ffdfeda9e2262211dd8fc77b79e3
won_chan <83020544+boychan112@users.noreply.github.com>
2021-09-02 23:43:14 +0900

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -82,7 +82,7 @@ async def on_message(message):
         await message.channel.send(embed=embed)
 
     if content.startswith("!시정표"):
-        embed=discord.Embed(description="<1교시>\n09:10 ~ 10:00\n<2교시>\n10:10 ~ 11:00\n<3교시>\n11:10 ~ 12:00\n<4교시>\n12:10 ~ 1:00\n<5교시>\n02:00 ~ 2:50\n<6교시>\n03:00 ~ 3:50\n<7교시>\n04:00 ~ 4:50", color=0x00ff56)
+        embed=discord.Embed(description="<1교시>\n09:10 ~ 10:00\n\n<2교시>\n10:10 ~ 11:00\n\n<3교시>\n11:10 ~ 12:00\n\n<4교시>\n12:10 ~ 1:00\n\n<5교시>\n02:00 ~ 2:50\n\n<6교시>\n03:00 ~ 3:50\n\n<7교시>\n04:00 ~ 4:50", color=0x00ff56)
         embed.set_author(name="수원정보과학고등학교 시정표", url=" ")
         await message.channel.send(embed=embed)
         
