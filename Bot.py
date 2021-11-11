ff737d32abd20cbe3d82f551c10eac8e4d1e1a93
won_chan <83020544+boychan112@users.noreply.github.com>
2021-11-11 11:26:33 +0900

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -92,7 +92,7 @@ async def on_message(message):
         await message.channel.send(embed=embed)
 
     if content.startswith("!30분 단축"):
-        embed=discord.Embed(description="<1교시>\n09:10 ~ 09:40\n\n<2교시>\n09:50 ~ 10:20\n\n<3교시>\n10:30 ~ 11:00\n\n<4교시>\n11:10 ~ 11:40\n\n<5교시>\n12:40 ~ 1:20\n\n<6교시>\n1:30 ~ 2:00\n\n<7교시>\n02:10 ~ 2:40", color=0x00ff56)
+        embed=discord.Embed(description="<1교시>\n09:10 ~ 09:40\n\n<2교시>\n09:50 ~ 10:20\n\n<3교시>\n10:30 ~ 11:00\n\n<4교시>\n11:10 ~ 11:40\n\n<5교시>\n12:40 ~ 1:10\n<6교시>\n1:20 ~ 1:50\n\n<7교시>\n02:00 ~ 2:30", color=0x00ff56)
         embed.set_author(name="수원정보과학고등학교 시정표", url=" ")
         await message.channel.send(embed=embed)
 
